07738be867f9c17064b7ec34b37ff84d24a1e7af
iustin <iustin.neaga@cherrychain.it>
2024-07-01 14:07:46 +0200

    first commit. Added NavBar for app

---
+++ app/src/main/java/com/example/parthfinder/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.parthfinder
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.parthfinder.theme.ParthFinderTheme
+import com.example.parthfinder.ui.screen.CampainScreen
+import com.example.parthfinder.ui.screen.CharactersScreen
+import com.example.parthfinder.ui.screen.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navBarController = rememberNavController()
+            ParthFinderTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("ParthFinder")
+                            }
+                        )
+                    }
+
+                ){ Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxSize().padding(100.dp)) {
+                        NavHost(
+                            navController = navBarController,
+                            startDestination = MainRoute.Home.name,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            composable(MainRoute.Home.name) { HomeScreen() }
+                            composable(MainRoute.Characters.name) { CharactersScreen() }
+                            composable(MainRoute.Campains.name) { CampainScreen() }
+                        }
+                    }
+                    Row(verticalAlignment = Alignment.Bottom, modifier = Modifier.fillMaxSize()) {
+                        Navbar(navBarController)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Navbar(navBarController: NavHostController)
+{
+    NavigationBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.1f)
+    ) {
+        NavigationBarItem(
+            label = { Text(text = "Campagne") },
+            selected = false,
+            onClick = { navBarController.navigate(MainRoute.Campains.name) },
+            icon = { Icon(modifier = Modifier.fillMaxHeight(0.3f), painter = painterResource(id = R.drawable.campagne_button), contentDescription = "campagne") }
+        )
+        NavigationBarItem(
+            label = { Text(text = "Home") },
+            selected = false,
+            onClick = { navBarController.navigate(MainRoute.Home.name) },
+            icon = { Icon(modifier = Modifier.fillMaxHeight(0.3f), painter = painterResource(id = R.drawable.home_button), contentDescription = "home") }
+        )
+        NavigationBarItem(
+            label = { Text(text = "Personaggi") },
+            selected = false,
+            onClick = { navBarController.navigate(MainRoute.Characters.name) },
+            icon = { Icon(modifier = Modifier.fillMaxHeight(0.3f), painter = painterResource(id = R.drawable.personaggi_button), contentDescription = "personaggi") }
+
+        )
+    }
+}
+
+enum class MainRoute {
+    Home,
+    Characters,
+    Campains
+}
